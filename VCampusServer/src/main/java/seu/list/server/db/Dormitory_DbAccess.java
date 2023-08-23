b477b41601e0901fa1d243079d0f41221451f143
RyanZhao <437071476@qq.com>
2023-08-23 10:45:43 +0800

    第一版？ maven打包后整个target是灰的 图片，按钮资源没放对位置，加载不出来 数据源没配置

---
+++ VCampusServer/src/main/java/seu/list/server/db/Dormitory_DbAccess.java
@@ -0,0 +1,47 @@
+package seu.list.server.db;
+
+import java.sql.*;
+
+public class Dormitory_DbAccess {
+
+	Connection conn = null;
+	Statement stmt = null;
+	ResultSet rs=null;
+	
+	public Connection getConnection() {
+		try {
+			// 装载驱动
+			
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+			String dbpath = "src\\main\\resources\\vCampus.accdb";
+			String user="";
+			String passwd="";
+			// 建立连接
+			String url = "jdbc:ucanaccess://"+dbpath;
+			conn = DriverManager.getConnection(url,user,passwd);
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+   			
+	public void closeConnection(Connection con,ResultSet rs,Statement s) {
+		try {
+			if (s!=null) {
+				s.close();
+			}
+			if (con!=null) {
+				con.close();	
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
